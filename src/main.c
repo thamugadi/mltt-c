99e58ba8b70d4cc6011569ae276c73b77dc56b2c
aramya <22577625+thamugadi@users.noreply.github.com>
2025-04-04 03:54:35 +0000

    todo

--- src/main.c
+++ src/main.c
@@ -19,6 +19,7 @@ extern def_env_t sym_hashmap[HASH_SIZE];
 // this evaluates the whole program by dispatching blocks to parse_and_eval_block
 char* eval_program(char* str, int size)
 {
+  // todo: rewrite this, optimized
   str[size] = 0;
   remove_comments(str);
   char** blocks = malloc(sizeof(char*));
@@ -37,6 +38,7 @@ char* eval_program(char* str, int size)
     }
     i++;
   }
+  
   size_t len = 0;
   for (int j = 0; j < i; j++) len += strlen(blocks[j]);
   char* result = malloc(len + 1);
@@ -53,11 +55,12 @@ void free_globals()
 {
   for (int i = 0; i < HASH_SIZE; i++) free_def_entry(&sym_hashmap[i], false);
 }
+
+// todo: add multiple possible operations to do, not only loading a file
 int main(int argc, char** argv)
 {
   char* str;
   long size;
-  size_t read;
   if (argc != 2)
   {
     return 0;
@@ -68,35 +71,22 @@ int main(int argc, char** argv)
     perror("no such file");
     return EXIT_FAILURE;
   }
-  if (fseek(fp, 0, SEEK_END) != 0)
-  {
-    fclose(fp);
-    return EXIT_FAILURE;
-  }
+  fseek(fp, 0, SEEK_END); 
   size = ftell(fp);
-  if (size < 0)
+  if (size < 7)
   {
     fclose(fp);
-    return EXIT_FAILURE;
+    printf("something went wrong\n");
+    return 0;
   }
   rewind(fp);
   str = malloc(size + 1);
-  if (str == NULL)
-  {
-    fclose(fp);
-    return EXIT_FAILURE;
-  }
-  read = fread(str, 1, size, fp);
-  if (read != (size_t)size)
-  {
-    free(str);
-    fclose(fp);
-    return EXIT_FAILURE;
-  }
-  
+  fread(str, 1, size, fp);
+
   init_hashmap();
   char* eval = eval_program(str, size);
   printf("%s", eval);
+
   free(eval);
   fclose(fp);
   free_globals();
